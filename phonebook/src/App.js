ac329813e2244a98db7f9a2311a2283fdaed6444
Naziru Abdussalam Ibrahim <naziruabdussalami@gmail.com>
2022-03-10 21:47:01 +0000

    Add part2

---
+++ phonebook/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('') 
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+  
+    setPersons(persons.concat(nameObject));
+    setNewName('')
+  }
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: {' '}
+          <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>  
+      <h2>Numbers</h2>
+      {persons.map((person)=> 
+        <div key={person}>{person.name}</div>
+        )}
+    </div>
+  )
+}
+
+
+export default App;
